1f39e5ecd91a5cb5d10e24b4d88fd87e10166ee1
todoreactapp <aleksandr.radchenko.00@mail.ru>
2020-05-04 00:39:00 +0700

    'react-native-demo'

--- App.js
+++ App.js
@@ -1,19 +1,94 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import { NavBar } from "./src/components/NavBar";
+import { MainScreen } from "./src/screens/MainScreen";
+import { TodoScreen } from "./src/screens/TodoScreen";
 
 export default function App() {
+  const [todoId, setTodoId] = useState(null);
+  const [todos, setTodos] = useState([
+    { id: "1", title: "Когда же это кончится" },
+    { id: "2", title: "Быстрей бы весна" },
+    { id: "3", title: "А потом сессия..." }
+  ]);
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ]);
+  };
+
+  const removeTodo = id => {
+    const todo = todos.find(t => t.id === id);
+    Alert.alert(
+      "Удаление элемента",
+      `Вы уверены, что хотите удалить ${todo.title}`,
+      [
+        {
+          text: "Отмена",
+          style: "cancel"
+        },
+        {
+          text: "Удалить",
+          onPress: () => {
+            setTodoId(null);
+            setTodos(prev => prev.filter(todo => todo.id !== id));
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const updateTodo = (id, title) => {
+    setTodos(old =>
+      old.map(todo => {
+        if (todo.id === id) {
+          todo.title = title;
+        }
+        return todo;
+      })
+    );
+  };
+
+  let content = (
+    <MainScreen
+    todos={todos}
+    addTodo={addTodo}
+    removeTodo={removeTodo}
+    openTodo={setTodoId}
+    />
+  );
+
+  if (todoId) {
+    const selectedTodo = todos.find(todo => todo.id === todoId);
+    content = (
+      <TodoScreen
+      goBack={() => {
+        setTodoId(null);
+      }}
+      todo={selectedTodo}
+      onRemove={removeTodo}
+      onSave={updateTodo}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <NavBar title="Todo App" />
+      <View style={styles.container}>{content}</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  }
 });
---
+++ src/components/EditModal.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button, Modal, Alert, ToolbarAndroidComponent } from "react-native";
+
+export const EditModal = ({ visible, onCancel, value, onSave }) => {
+    const [title, setTitle] = useState(value);
+    const saveHandler = () => {
+        if (title.trim().length < 3) {
+            Alert.alert("Ошибка!", `Сейчас длина составляет ${title.trim().length}`)
+        } else {
+            onSave(title);
+        }
+    };
+    return (
+        <Modal visible={visible} animationType='slide' transparent={false}>
+            <View style={styles.wrap}>
+                <TextInput
+                value={title}
+                onChangeText={setTitle}
+                style={styles.input}
+                placeholder="Введите название"
+                maxLength={64}
+                />
+                <View style={styles.buttons}>
+                    <Button title="Отменить" color="#e53935" onPress={onCancel} />
+                    <Button title="Сохранить" onPress={saveHandler} />
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    wrap: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    input: {
+        padding: 10,
+        borderBottomColor: "#3949ab",
+        borderBottomWidth: 2,
+        width: "80%"
+    },
+    buttons: {
+        width: "100%",
+        marginTop: 10,
+        flexDirection: "row",
+        justifyContent: "space-around"
+    }
+});
